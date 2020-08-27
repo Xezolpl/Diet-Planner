e5fd17c70fee1eec4d52d4038215c4af19025b6e
XezolPL <XezolPL@wp.pl>
2020-08-27 12:31:45 +0200

    Nice looking date selector in the meal plan view, with the date changing mechanic

--- lib/util/xdatetime.dart
+++ lib/util/xdatetime.dart
@@ -48,4 +48,23 @@ extension XDateTime on DateTime {
     else if (this.isTomorrow()) return 'Tomorrow';
     return this.toDayMonthYearFormat();
   }
+
+  String printWeekday() {
+    switch (this.weekday) {
+      case 1:
+        return 'Monday';
+      case 2:
+        return 'Tuesday';
+      case 3:
+        return 'Wednesday';
+      case 4:
+        return 'Thursday';
+      case 5:
+        return 'Friday';
+      case 6:
+        return 'Saturday';
+      case 7:
+        return 'Sunday';
+    }
+  }
 }
