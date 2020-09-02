c3f9794e2e41d6e4d9fcdc8107ca88c98af6b75a
XezolPL <XezolPL@wp.pl>
2020-09-02 21:36:55 +0200

    Diet markers

--- lib/presentation/diets/diet_card.dart
+++ lib/presentation/diets/diet_card.dart
@@ -24,6 +24,7 @@ class DietCard extends StatelessWidget {
             Expanded(
               flex: 2,
               child: Container(
+                padding: EdgeInsets.all(3),
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(10),
                   image: DecorationImage(
@@ -34,55 +35,75 @@ class DietCard extends StatelessWidget {
                         'assets/png/diet_backgrounds/fruit_diet.png',
                       )),
                 ),
-                padding: EdgeInsets.all(10),
                 alignment: Alignment.center,
-                child: AutoSizeText(
-                  diet.name,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 26,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w900,
-                    shadows: [
-                      Shadow(color: Color(0xff333333), blurRadius: 5),
-                      Shadow(
-                          color: Colors.black,
-                          offset: Offset(2, 2),
-                          blurRadius: 1.5)
-                    ],
-                  ),
+                child: Stack(
+                  children: [
+                    Align(
+                        alignment: Alignment.bottomLeft,
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              InformationMark(
+                                  iconData: FontAwesomeIcons.clock,
+                                  text: '3h',
+                                  color: Colors.amber[700]),
+                              InformationMark(
+                                  iconData: FontAwesomeIcons.coins,
+                                  text: '40zł',
+                                  color: Colors.blue[800]),
+                              SizedBox(),
+                            ])),
+                    Container(
+                      padding: EdgeInsets.only(bottom: 30, top: 20),
+                      alignment: Alignment.center,
+                      child: AutoSizeText(
+                        diet.name,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 26,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w900,
+                          shadows: [
+                            Shadow(color: Color(0xff333333), blurRadius: 5),
+                            Shadow(
+                                color: Colors.black,
+                                offset: Offset(2, 2),
+                                blurRadius: 1.5)
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                padding: EdgeInsets.all(5),
-                height: 150,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10), color: lightGrey),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    IconWithTextRow(
-                        iconData: FontAwesomeIcons.rocket,
-                        iconColor: Colors.green,
-                        label: '3120kcal'),
-                    IconWithTextRow(
-                        iconData: FontAwesomeIcons.fire,
-                        iconColor: Colors.red,
-                        label: '500g'),
-                    IconWithTextRow(
-                        iconData: FontAwesomeIcons.dumbbell,
-                        iconColor: Colors.blueAccent,
-                        label: '120g'),
-                    IconWithTextRow(
-                        iconData: FontAwesomeIcons.oilCan,
-                        iconColor: Colors.yellow[700],
-                        label: '80g'),
-                  ],
-                ),
+            Container(
+              width: 110,
+              padding: EdgeInsets.all(5),
+              height: 150,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10), color: lightGrey),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconWithTextRow(
+                      iconData: FontAwesomeIcons.rocket,
+                      iconColor: Colors.green,
+                      label: '3120kcal'),
+                  IconWithTextRow(
+                      iconData: FontAwesomeIcons.fire,
+                      iconColor: Colors.red,
+                      label: '500g'),
+                  IconWithTextRow(
+                      iconData: FontAwesomeIcons.dumbbell,
+                      iconColor: Colors.blueAccent,
+                      label: '120g'),
+                  IconWithTextRow(
+                      iconData: FontAwesomeIcons.oilCan,
+                      iconColor: Colors.yellow[700],
+                      label: '80g'),
+                ],
               ),
             )
           ],
@@ -120,6 +141,45 @@ class IconWithTextRow extends StatelessWidget {
             width: 10,
           ),
           Text(label, style: TextStyle(fontWeight: FontWeight.w500)),
+        ],
+      ),
+    );
+  }
+}
+
+class InformationMark extends StatelessWidget {
+  final IconData iconData;
+  final String text;
+  final Color color;
+  const InformationMark(
+      {@required this.iconData,
+      @required this.text,
+      @required this.color,
+      Key key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 3, horizontal: 7),
+      decoration:
+          BoxDecoration(color: color, borderRadius: BorderRadius.circular(5)),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(
+            iconData,
+            size: 20,
+            color: Colors.white,
+          ),
+          SizedBox(
+            width: 5,
+          ),
+          Text(text,
+              style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold)),
         ],
       ),
     );
