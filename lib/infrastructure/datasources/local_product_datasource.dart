7a701f50ed7df2d87e391270aef7c34fab069915
XezolPL <xezolpl@wp.pl>
2020-09-13 16:08:25 +0200

    Logging messages added to all of the infrastructure classes and methods.

--- lib/infrastructure/datasources/local_product_datasource.dart
+++ lib/infrastructure/datasources/local_product_datasource.dart
@@ -1,6 +1,5 @@
 import 'dart:developer';
 
-import 'package:diet_planner/core/error/exceptions.dart';
 import 'package:diet_planner/core/params.dart';
 import 'package:diet_planner/domain/entities/product.dart';
 import 'package:diet_planner/infrastructure/local_database.dart';
@@ -30,35 +29,40 @@ class LocalProductDataSourceImpl implements ILocalProductDataSource {
 
   @override
   Future<void> insert(Product product) {
+    log('Inserting product: $product');
     return database.insert(PRODUCTS_TABLE, product.toJson());
   }
 
   @override
   Future<void> update(Product product) {
+    log('Updating product: $product');
     return database.update(PRODUCTS_TABLE, product.toJson(),
         where: 'id = ?', whereArgs: [product.id]);
   }
 
   @override
   Future<void> delete(Product product) {
+    log('Deleting product: $product');
     return database
         .delete(PRODUCTS_TABLE, where: 'id = ?', whereArgs: [product.id]);
   }
 
   @override
   Future<List<Product>> getAllProducts() async {
+    log('Get all products called');
     List<Map<String, dynamic>> records = await database.query(PRODUCTS_TABLE);
     return Future.value(records.map((json) => Product.fromJson(json)).toList());
   }
 
   @override
   Future<Product> getProduct(ProductDatabaseParams params) async {
+    log('Getting concrete product for params: $params');
     return await database
         .query(PRODUCTS_TABLE,
             where: params.getWhere(), whereArgs: params.getWhereArgs())
         .then<Product>((products) {
       //Its called products because of query syntax but there is allways only one product
-      log('Get product from SQLite database by ID: ' + products.toString());
+      log('Getting concrete product succed with: ' + products.toString());
       return Product.fromJson(products.first);
     });
   }
@@ -68,10 +72,9 @@ class LocalProductDataSourceImpl implements ILocalProductDataSource {
     database.query(PRODUCTS_TABLE,
         where: 'id = ?', whereArgs: [product.id]).then((products) {
       //Its called products because of query syntax but there is allways only one product
-      log('Products in database with params: id = ${product.id}' +
-          products.toString());
+      log('Check or cache the product called for $product');
       if (products.length == 0) {
-        //cache
+        log('Cache product $product');
         insert(product);
       }
     });
@@ -79,10 +82,13 @@ class LocalProductDataSourceImpl implements ILocalProductDataSource {
 
   @override
   Future<List<Product>> searchForProducts(ProductQueryParams params) {
+    log('Search for products called for params: $params');
     return database
         .query(PRODUCTS_TABLE,
             where: params.getWhere(), whereArgs: params.getWhereArgs())
-        .then<List<Product>>((products) =>
-            products.map((json) => Product.fromJson(json)).toList());
+        .then<List<Product>>((products) {
+      log('Products found $products');
+      return products.map((json) => Product.fromJson(json)).toList();
+    });
   }
 }
--- lib/infrastructure/repositories/product_repository_impl.dart
+++ lib/infrastructure/repositories/product_repository_impl.dart
@@ -13,8 +13,13 @@ import 'package:diet_planner/infrastructure/datasources/local_product_datasource
 import 'package:diet_planner/infrastructure/local_database.dart';
 import 'package:injectable/injectable.dart';
 
+///Typedef used to distinc the product is from database (server) or api
 typedef Future<Product> ProductFromDatabaseOrApi();
 
+///Implementation of IProductRepository
+///It is a class used to communication between product BLoCs and DataSources.
+///It gets the data from data sources and returns them as Futures of Either<Failure, ?>
+///To create that repository in code prefferably use getIt<IProductRepository>()
 @Injectable(as: IProductRepository)
 class ProductRepositoryImpl implements IProductRepository {
   final IApiProductDataSource apiProductDataSource;
@@ -28,41 +33,53 @@ class ProductRepositoryImpl implements IProductRepository {
       this.localProductDataSource,
       this.networkInfo);
 
+  ///Shortcut for getProductFromRemote and getProductFromApi functions
   Future<Either<Failure, Product>> _getProductFromDbOrApi(
       ProductDatabaseParams params,
       ProductFromDatabaseOrApi getFromDbOrApi) async {
     if (await networkInfo.isConnected) {
+      //if online
       try {
         final product = await getFromDbOrApi();
+        //if product is not cached to local database - cache it
         localProductDataSource.checkOrCache(product);
+        //if succeed - return the product
         return Right(product);
       } on ApiException catch (e) {
-        return Left(ApiFailure(
-            'API threw exception during getting product with params: ${params.toString()}. Error: ${e.toString()}}'));
+        //If something got wrong in the API
+        log('ApiException caught in getProductFromApi(). Error: $e');
+        return Left(ApiFailure());
+      } on ServerException catch (e) {
+        //If something got wrong on server database
+        log('ServerException caught in getProductFromRemote(). Error: $e');
+        return Left(ServerFailure());
       } on DatabaseException catch (e) {
-        return Left(CacheFailure(
-            'Local database threw exception while checkOrCache(product) method. Error: ${e.toString()}'));
+        //If something got wrong in local database
+        log('DatabaseException caught in checkOrCache(). Error: $e');
+        return Left(CacheFailure());
       }
     } else {
+      //if offline
       return await getProductLocal(params);
     }
   }
 
-  ///It is used to get concrete product if we have internet connection
+  ///It is used to get concrete product from API.
+  ///If offline it will search for product in local database
   @override
   Future<Either<Failure, Product>> getProductFromApi(int barcode) async {
+    log('Getting product from API for barcode: $barcode');
     return await _getProductFromDbOrApi(ProductDatabaseParams(barcode: barcode),
         () {
       return apiProductDataSource.getProduct(barcode);
     });
   }
 
-  ///It could be used in only two ways
-  ///1. To get e.g. Breakfast from database to the app.
-  ///2. To get product from section "Yours products"
+  ///It is used to get product from server database.
   @override
   Future<Either<Failure, Product>> getProductRemote(
       ProductDatabaseParams params) async {
+    log('Getting product from server database for params: $params');
     return await _getProductFromDbOrApi(params, () {
       return remoteProductDataSource.getProduct(params);
     });
@@ -76,8 +93,8 @@ class ProductRepositoryImpl implements IProductRepository {
       final product = await localProductDataSource.getProduct(params);
       return Right(product);
     } on DatabaseException catch (e) {
-      return Left(CacheFailure(
-          'Local database threw exception while getProduct(params) method with params: ${params.toString()}. Error: ${e.toString()}'));
+      log('DatabaseException caught in getProductLocal(). Error: $e');
+      return Left(CacheFailure());
     }
   }
 
@@ -97,23 +114,46 @@ class ProductRepositoryImpl implements IProductRepository {
         //TODOawait remoteProductDataSource.insert(product);
       }
       return Right(unit);
-    } on DatabaseException {
-      return Left(CacheFailure(
-          'Product ${product.toString()} actually exists in SQLite db.'));
-    } on ServerException {
-      return Left(ServerFailure('')); //TODO FAILURE
+    } on DatabaseException catch (e) {
+      log('DatabaseException caught in insertProduct(). Error: $e');
+      return Left(CacheFailure());
+    } on ServerException catch (e) {
+      log('ServerException caught in insertProduct(). Error: $e');
+      return Left(ServerFailure());
     }
   }
 
   @override
-  Future<Either<Failure, Unit>> updateProduct(Product product) {
-    // TODO: implement updateProduct
-    throw UnimplementedError();
+  Future<Either<Failure, Unit>> updateProduct(Product product) async {
+    try {
+      await localProductDataSource.update(product);
+      if (await networkInfo.isConnected) {
+        //TODOawait remoteProductDataSource.update(product);
+      }
+      return Right(unit);
+    } on DatabaseException catch (e) {
+      log('DatabaseException caught in updateProduct(). Error: $e');
+      return Left(CacheFailure());
+    } on ServerException catch (e) {
+      log('ServerException caught in updateProduct(). Error: $e');
+      return Left(ServerFailure());
+    }
   }
 
   @override
-  Future<Either<Failure, Unit>> deleteProduct(Product product) {
-    // TODO: implement deleteProduct
-    throw UnimplementedError();
+  Future<Either<Failure, Unit>> deleteProduct(Product product) async {
+    try {
+      await localProductDataSource.delete(product);
+      if (await networkInfo.isConnected) {
+        //TODOawait remoteProductDataSource.delete(product);
+      }
+      return Right(unit);
+    } on DatabaseException catch (e) {
+      log('DatabaseException caught in deleteProduct(). Error: $e');
+      return Left(CacheFailure());
+    } on ServerException catch (e) {
+      log('ServerException caught in deleteProduct(). Error: $e');
+      return Left(ServerFailure());
+    }
   }
 }
